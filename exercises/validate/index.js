eb464bc845b978c5090835fb4ffc55a854488b4b
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-28 18:20:05 -0400

    completed validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,10 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = -Infinity, max = Infinity) {
+    if(!node) return true;
+    if(node.data < min || node.data > max) return false;
+    return validate(node.left, min, node.data) && validate(node.right, node.data, max)
+}
 
 module.exports = validate;
