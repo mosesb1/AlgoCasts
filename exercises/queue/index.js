9ebc0f13ad99d081fd23014bd8f11f068db4af61
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-28 17:41:37 -0400

    completed queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -7,7 +7,32 @@
 //     const q = new Queue();
 //     q.add(1);
 //     q.remove(); // returns 1;
-
-class Queue {}
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null
+    }
+}
+class Queue {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    add(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+    }
+    remove(){
+        if(!this.head) return undefined;
+        let removedNode = this.head;
+        this.head = removedNode.next;
+        return removedNode.val;
+    }
+}
 
 module.exports = Queue;
