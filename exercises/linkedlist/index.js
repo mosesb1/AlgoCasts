a8843e7cda4dbdde77d99c283b36548f27c03381
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-28 13:23:25 -0400

    completed linkedlist and matrix

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,37 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+    insertFirst(data){
+        const newNode = new Node(data);
+        if(this.head){
+            let head = this.head;
+            newNode.next = head;
+        }
+        this.head = newNode;
+    }
+    size(){
+        let counter = 0;
+        let curr = this.head;
+        while(curr){
+            counter++;
+            curr = curr.next;
+        }
+        return counter;
+    }
+    getFirst(){
+        return this.head;
+    }
+}
 
 module.exports = { Node, LinkedList };
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,40 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let curr = 1;
+    const matrix = [];
+    for(let i = 0; i < n; i++){
+        matrix.push(new Array(n))
+    }
+    let rowStart = 0, rowEnd = matrix.length, colStart = 0, colEnd = matrix[0].length;
+    while(rowStart < rowEnd && colStart < colEnd){
+        if(rowStart === rowEnd){
+            matrix[rowStart][colStart] = curr;
+            break;
+        }
+        for(let i = colStart; i < colEnd; i++){
+            matrix[rowStart][i] = curr;
+            curr++;
+        }
+        for(let i = rowStart+1; i < rowEnd; i++){
+            matrix[i][colEnd-1] = curr;
+            curr++;
+        }
+        for(let i = colEnd-2; i >= colStart; i--){
+            matrix[rowEnd-1][i] = curr;
+            curr++;
+        }
+        for(let i = rowEnd-2; i > rowStart; i--){
+            matrix[i][colStart] = curr;
+            curr++
+        }
+        rowStart++;
+        colStart++;
+        colEnd--;
+        rowEnd--;
+    }
+    return matrix;
+}
 
 module.exports = matrix;
